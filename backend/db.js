be41b205ae17faf84474c3fa7a75035184fa84c3
Rozali Kaiss <kaissrozali79@gmail.com>
2025-12-21 19:33:05 +0200

    fix mysqp railway connection

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({// get info from database eza fe she ghlt send error message 
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+connection.connect((err) => {
+  if (err) {
+    console.error("DB error:", err);
+    return;
+  }
+  console.log("Connected to MySQL");
 });
+
+module.exports = connection;
 
 connection.connect((err) => {
   if (err) {
