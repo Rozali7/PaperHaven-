e629db22e2d6a007fbedfb9ff2678380aea554d3
Rozali Kaiss <kaissrozali79@gmail.com>
2025-12-22 21:21:10 +0200

    use database url

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,11 @@
-
 require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection(process.env.DATABASE_URL);
+
 db.connect((err) => {
-  if (err) {
-    console.error("❌ MySQL connection failed:", err);
-  } else {
-    console.log("✅ MySQL connected");
-  }
+  if (err) console.error("❌ MySQL connection failed:", err);
+  else console.log("✅ MySQL connected");
 });
 
 module.exports = db;
