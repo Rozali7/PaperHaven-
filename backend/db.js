da831d35684c94c60d45fe264ab728b915661259
Rozali Kaiss <kaissrozali79@gmail.com>
2025-12-23 12:34:47 +0200

    fix port db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection(process.env.DATABASE_URL || process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) console.error("❌ MySQL connection failed:", err);
