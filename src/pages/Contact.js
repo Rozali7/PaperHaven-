c7f33c301dd5b0671d1a8be32862c2ecb87ce118
Rozali Kaiss <kaissrozali79@gmail.com>
2025-12-23 17:03:53 +0200

    fix the contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -24,7 +24,7 @@ export default function Contact() {
       setLoading(true); //disable button while sending
 
       //send message to backend (so it can be inserted into MySQL messages table)
-      const res = await fetch(`${API_URL}/api/contact`, {
+      const res = await fetch(`${API_URL}/api/contacts`, {
         method: "POST", //POST = send data to server
         headers: { "Content-Type": "application/json" }, //we are sending JSON
         body: JSON.stringify({
