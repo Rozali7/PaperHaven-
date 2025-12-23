03a66255097567b31a96862c638251243f41f799
Rozali Kaiss <kaissrozali79@gmail.com>
2025-12-23 14:16:10 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://paperhaven-production-773e.up.railway.app", // ✅ FRONTEND (exact)
+      "https://paperhaven-production.up.railway.app", // ✅ FRONTEND (exact)
       "http://localhost:3000"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
